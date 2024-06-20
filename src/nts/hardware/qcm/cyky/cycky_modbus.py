d33fb5a8edf43acfea149897ca420d7db1f190d5
bond_anton <bond.anton@gmail.com>
2024-06-20 13:07:09 +0300

    PyModbus support

--- src/nts/hardware/qcm/cyky/cycky_modbus.py
+++ src/nts/hardware/qcm/cyky/cycky_modbus.py
@@ -41,7 +41,7 @@ class QTMModbus(QTM):
                 # skip start and stop bytes and parse as a hex string
                 payload = (
                     struct.pack(">BB", response.slave_id, response.function_code)
-                    + response.encode()
+                    + response.encode() + b"\x00"
                 )
                 return payload
             if verbose:
